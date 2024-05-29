00b4cab468acb570cc0610c38aa707f95b26b0fd
pengHTYX <1054393196@qq.com>
2024-05-30 01:09:59 +0800

    update_project_page

--- instant-nsr-pl/run.sh
+++
@@ -1,5 +0,0 @@
-python launch.py \
-    --config configs/neuralangelo-ortho-wmask.yaml \
-    --gpu $1 \
-    --train dataset.root_dir=../mv_res dataset.scene=$2 \
-    --exp_dir $3 
--- instant-nsr-pl/run_test.sh
+++
@@ -1,4 +0,0 @@
-python launch.py --config configs/neuralangelo-ortho-wmask.yaml --gpu 0 --test dataset.root_dir='mv_res/steplr/webcase'\
-    dataset.scene=backpack \
-    --resume 'exp/moria-000-135/@20240311-200730/ckpt/epoch=0-step=3000.ckpt' \
-    --exp_dir exp_demo
